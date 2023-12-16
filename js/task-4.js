caef9267618d05c1ee10b95c3ed6ffb54a8fb6cc
DashaAndreeva <143691908+DashaAndreeva@users.noreply.github.com>
2023-12-16 20:08:04 +0200

    change-name

--- js/task-4.js
+++ js/task-4.js
@@ -9,7 +9,7 @@ const getTotalBalanceByGender = (users, gender) => {
   return userBalance;
 };
 
-const allUsers = [
+const allUsers1 = [
   {
     name: "Moore Hensley",
     gender: "male",
@@ -47,6 +47,6 @@ const allUsers = [
   },
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsers1, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsers1, "female")); // 8863
